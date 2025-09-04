aeda3eff82039e9d5e63292b716262c959432369
Namhehe237 <namnguyen23072003@gmail.com>
2025-09-04 22:43:28 +0700

    Config cors

--- exam-test/src/main/java/com/example/demo/examOnline/configuration/SecurityConfig.java
+++ exam-test/src/main/java/com/example/demo/examOnline/configuration/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
