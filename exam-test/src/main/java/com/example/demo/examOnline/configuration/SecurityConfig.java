077c7c5f004ad7709048358e8ae29f4abb25c858
Namhehe237 <namnguyen23072003@gmail.com>
2025-09-04 22:43:45 +0700

    Config cors

--- exam-test/src/main/java/com/example/demo/examOnline/configuration/SecurityConfig.java
+++ exam-test/src/main/java/com/example/demo/examOnline/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.examOnline.configuration;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
